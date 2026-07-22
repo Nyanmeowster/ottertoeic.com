df2736fe8d4a8820085957b5dc596e44f6e28a41
Nyanmeowster <cfspusa@gmail.com>
2026-07-22 11:12:11 +0800

    Publish GitHub Pages at root URL

--- vite.pages.config.ts
+++ vite.pages.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   root: "gh-pages-app",
-  base: "/ottertoeic.com/",
+  base: "/",
   publicDir: "../public",
   plugins: [react()],
   build: {
